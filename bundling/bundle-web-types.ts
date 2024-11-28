65d07d3d3b64db19f43ed6010e8c523f9786b0c5
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-11-28 10:02:29 +0100

    Update bundle-web-types.ts

--- bundling/bundle-web-types.ts
+++ bundling/bundle-web-types.ts
@@ -2,7 +2,7 @@ import { bundle } from "https://deno.land/x/emit@0.28.0/mod.ts";
 import { createCache } from "https://deno.land/x/deno_cache@0.6.0/mod.ts";
 
 // Parse args
-const [release, source = `https://deno.land/x/grammy@${release}/mod.ts`] =
+const [release, source = `https://deno.land/x/grammy@${release}/types.ts`] =
     Deno.args;
 if (!release) throw new Error("No release specified!");
 
@@ -30,12 +30,12 @@ const { code: bundledCode } = await bundle(source, {
 console.log("Emitting ...");
 // Strip the huge inline source map which is somehow generated anyway
 await Deno.writeTextFile(
-    "../out/web.mjs",
+    "../out/web-types.mjs",
     bundledCode.replace(/\/\/# sourceMappingURL=.*\n/, ""),
 );
 await Deno.writeTextFile(
-    "../out/web.d.ts",
-    'export * from "./mod";\n',
+    "../out/web-types.d.ts",
+    'export * from "./types";\n',
 );
 
 console.log("Done.");
